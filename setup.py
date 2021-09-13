e7ad2c5ea1d91d6eeaa580b32eff3053a62ba249
Ahmad Samir <ahmadaking1@gmail.com>
2021-09-13 20:16:18 +0200

    first directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a set of features extracted from the shape of the beans in images and it's required to predict the type of each bean. There are 7 bean types in this dataset.',
+    author='Ahmad Samir',
+    license='',
+)
